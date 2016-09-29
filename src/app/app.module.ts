38b93a8774d3607003468a6bfe1a175f2197780e
Bilo Lwabona <bilo.lwabona@gmail.com>
2016-09-30 00:57:44 +0200

    rearranged imports in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule }      from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { routing } from './app.routing';
-// Services
-import { AppService } from './app.service';
-import { GeocoderService } from './shared/services/geocoder.service';
-// Ws Components
+// Ws Component Library
 import { WsModule } from './shared/ws/ws.module';
 import { WsDemoModule } from './shared/ws-demo/ws-demo.module';
+// App Router
+import { routing } from './app.routing';
 // App Components
 import { AppComponent }   from './app.component';
 import { Component1 } from './components/component-1/component-1.component';
 import { Component2 } from './components/component-2/component-2.component';
+// App Services
+import { AppService } from './app.service';
+import { GeocoderService } from './shared/services/geocoder.service';
 
 @NgModule({
     imports: [
@@ -23,14 +24,14 @@ import { Component2 } from './components/component-2/component-2.component';
         WsDemoModule,
         routing
     ],
-    providers: [
-        AppService,
-        GeocoderService,
-    ],
     declarations: [
         AppComponent,
         Component1,
         Component2,
+    ],
+    providers: [
+        AppService,
+        GeocoderService,
     ],
     bootstrap: [AppComponent]
 })
